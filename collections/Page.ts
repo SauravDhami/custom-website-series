45eba4ad06c8c08d679e648ddc6c305c2d0b1f46
unknown <saugatdhami88@gmail.com>
2024-11-19 18:01:25 +0545

    project setup with required pages

--- collections/Page.ts
+++ collections/Page.ts
@@ -7,6 +7,8 @@ import { CallToAction } from '../blocks/CallToAction/Config';
 import { Type as CallToActionType } from '../blocks/CallToAction/Component';
 import { Content } from '../blocks/Content/Config';
 import { Type as ContentType } from '../blocks/Content/Component';
+import slug from '../fields/slug';
+import meta from '../fields/meta';
 
 
 export type Layout = CallToActionType | ContentType | ImageType
@@ -39,12 +41,6 @@ export const Page: CollectionConfig = {
       required: true,
     },
     {
-      name: 'image',
-      label: 'Featured Image',
-      type: 'upload',
-      relationTo: 'media',
-    },
-    {
       name: 'layout',
       label: 'Page Layout',
       type: 'blocks',
@@ -55,41 +51,8 @@ export const Page: CollectionConfig = {
         Image,
       ],
     },
-    {
-      name: 'meta',
-      label: 'Page Meta',
-      type: 'group',
-      fields: [
-        {
-          name: 'title',
-          label: 'Title',
-          type: 'text',
-        },
-        {
-          name: 'description',
-          label: 'Description',
-          type: 'textarea',
-        },
-        {
-          name: 'keywords',
-          label: 'Keywords',
-          type: 'text',
-        },
-      ],
-    },
-    {
-      name: 'slug',
-      label: 'Page Slug',
-      type: 'text',
-      admin: {
-        position: 'sidebar',
-      },
-      hooks: {
-        beforeValidate: [
-          formatSlug('title'),
-        ],
-      },
-    },
+    meta,
+    slug,
   ],
 };
 
